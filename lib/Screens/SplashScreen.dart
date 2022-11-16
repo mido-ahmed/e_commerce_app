97b5fe7066d9307c8674fa49bac7d031172fdb27
Mohamed Ahmed Hassan <220190077@fa-hists.edu.eg>
2022-11-16 18:25:29 +0200

    Edit in access token

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -1,5 +1,9 @@
+import 'package:e_commerce_app/Screens/HomeScreen.dart';
+import 'package:e_commerce_app/Screens/LoginScreen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,9 +15,20 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 3), () {
-      print(DateTime.now());
-      Navigator.pushNamed(context, '/Login');
+    Future.delayed(Duration(milliseconds: 5000), () async {
+      final prefs = await SharedPreferences.getInstance();
+      final accesstoken = await prefs.get('user_access_token');
+      if (accesstoken == null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) {
+          return LoginScreen();
+        }), (route) => false);
+      } else {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) {
+          return HomeScreen();
+        }), (route) => false);
+      }
     });
     return Scaffold(
       backgroundColor: Colors.amber,
