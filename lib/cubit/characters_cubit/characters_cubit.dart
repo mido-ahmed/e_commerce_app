2333e3262d25154349e85566f323b485bc42a978
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-01-29 16:30:42 +0200

    delete all new files

--- lib/cubit/characters_cubit/characters_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'characters_state.dart';
-
-class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
-}
